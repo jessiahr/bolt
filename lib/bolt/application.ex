700f0b2e974a4f7bd10cc730d768959ac333f9e1
Jessiah Ratliff <jessiahr@gmail.com>
2017-07-05 18:12:24 -0700

    init

---
+++ lib/bolt/application.ex
@@ -0,0 +1,22 @@
+defmodule Bolt.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Bolt.Worker.start_link(arg1, arg2, arg3)
+      # worker(Bolt.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Bolt.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/bolt_test.exs
@@ -0,0 +1,8 @@
+defmodule BoltTest do
+  use ExUnit.Case
+  doctest Bolt
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
