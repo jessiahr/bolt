4187785554b9f427d8062623365be9a9d17d7086
Jessiah Ratliff <jessiahr@gmail.com>
2017-07-07 17:16:44 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bolt.Mixfile do
 
   def project do
     [app: :bolt,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      package: package(),
      description: description(),
