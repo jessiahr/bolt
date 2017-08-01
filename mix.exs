5516c835003f9695f8743411d34aa418543e2599
Jessiah Ratliff <jessiahr@gmail.com>
2017-08-01 16:39:47 -0700

    exclude node modules

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Bolt.Mixfile do
     # These are the default files included in the package
     [
       name: :bolt,
-      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+      files: ["lib", "priv/static", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Jessiah Ratliff"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/jessiahr/bolt"}
